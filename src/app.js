0bcad2f66a41bce833518a4eb0f4e63720eba39d
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2024-09-08 20:09:49 +0530

    Added necessary middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 import express from "express"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+app.use( express.static( "public" ) )
+
+app.use( express.json( {
+    limit: "10kb",
+} ) )
+
+app.use( express.urlencoded( {
+    extended: true,
+    limit: "10kb",
+} ) )
+
+app.use( cookieParser() )
 
 export default app
