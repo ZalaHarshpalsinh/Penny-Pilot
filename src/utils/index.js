429d32b6f437b70e55aa2f54300efa1a28cdf1ca
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2024-09-10 22:27:25 +0530

    Added cloudinary utility

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,12 @@
 import asyncHandler from "./asyncHandler.js"
 import ApiError from "./ApiError.js"
 import ApiResponse from "./ApiResponse.js"
+import { uploadOnCloudinary } from "./cludinary.js"
 
 export
 {
     asyncHandler,
     ApiError,
     ApiResponse,
+    uploadOnCloudinary,
 }
