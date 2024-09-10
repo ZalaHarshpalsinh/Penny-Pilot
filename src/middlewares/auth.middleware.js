a8a81c7c7f1a005fe56431eeea03bffe301275d8
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2024-09-10 23:52:09 +0530

    Created user controller

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,23 +4,29 @@ import { User } from "../db/models/in"
 
 const verifyJwt = asyncHandler( async ( req, res, next ) =>
 {
-    const token = req.cookies?.accessToken || req.header( "Authorization" )?.replace( "Bearer ", "" )
-    if ( !token )
+    try
     {
-        throw new ApiError( 401, "Unauthorized request" )
-    }
+        const token = req.cookies?.accessToken || req.header( "Authorization" )?.replace( "Bearer ", "" )
+        if ( !token )
+        {
+            throw new ApiError( 401, "Unauthorized request" )
+        }
 
-    const decodedToken = jwt.verify( token, process.env.ACCESS_TOKEN_SECRET )
+        const decodedToken = jwt.verify( token, process.env.ACCESS_TOKEN_SECRET )
 
-    const user = await User.findById( decodedToken?._id ).select( "-password -refreshToken" )
+        const user = await User.findById( decodedToken?._id ).select( "-password -refreshToken" )
 
-    if ( !user )
+        if ( !user )
+        {
+            throw new ApiError( 401, "Invalid Access Token" )
+        }
+
+        req.user = user;
+        next()
+    } catch ( error )
     {
-        throw new ApiError( 401, "Invalid Access Token" )
+        throw new ApiError( 401, error?.message || "Invalid access token" )
     }
-
-    req.user = user;
-    next()
 } )
 
 export default verifyJwt
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,6 @@ const router = Router();
 
 
 router.post( "/register" )
-
 router.post( "/login" )
 router.post( "/logout" )
 router.post( "/refresh-token" )
